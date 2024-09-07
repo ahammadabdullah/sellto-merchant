03bb62130340d5a3428bab8ec2034b87ccf4c49c
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2024-09-07 13:38:02 +0600

    updated

--- src/app/api/waitlist/count/route.ts
+++ src/app/api/waitlist/count/route.ts
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   const cnt = await prisma.waitList.count();
-  return NextResponse.json({ count: cnt }, { status: 200 });
+  return NextResponse.json({ count: cnt + 50 }, { status: 200 });
 }
